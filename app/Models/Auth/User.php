d6881aaa39811f85ca6f6aa1a21159d9f911e779
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2023-03-13 20:24:07 +0700

    backend

---
+++ app/Models/Auth/User.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Models\Auth;
+
+use Cartalyst\Sentinel\Users\EloquentUser;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Auth\Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject as AuthenticatableUserContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Passport\HasApiTokens;
+
+class User extends EloquentUser implements AuthenticatableUserContract, AuthenticatableContract // implements JWTSubject // Authenticatable implements JWTSubject
+{
+    use Authenticatable, HasApiTokens, SoftDeletes;
+    const last_login = null;
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'email',
+		'password',
+		'name',
+        'phone',
+        'user_type',
+        'created_by'
+    ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password'
+	];
+
+    public function user_role(){
+        return $this->hasOne(UserRole::class, 'user_id', 'id');
+    }
+
+    public function getCreatedAtAttribute($value)
+    {
+        if($value == null){
+            return '';
+        }
+        else{
+            return (new Carbon($value))->timezone('Asia/Jakarta')->toDateTimeString();
+        }
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        if($value == null){
+            return '';
+        }
+        else{
+            return (new Carbon($value))->timezone('Asia/Jakarta')->toDateTimeString();
+        }
+    }
+
+    public function getDeletedAtAttribute($value)
+    {
+        if($value == null){
+            return '';
+        }
+        else{
+            return (new Carbon($value))->timezone('Asia/Jakarta')->toDateTimeString();
+        }
+    }
+
+    public function getLastLoginAttribute($value)
+    {
+        if($value == null){
+            return '';
+        }
+        else{
+            return (new Carbon($value))->timezone('Asia/Jakarta')->toDateTimeString();
+        }
+    }
+
+    public function getEmailVerifiedAtAttribute($value)
+    {
+        if($value == null){
+            return '';
+        }
+        else{
+            return (new Carbon($value))->timezone('Asia/Jakarta')->toDateTimeString();
+        }
+    }
+
+    public function getOtpExpiredAttribute($value)
+    {
+        if($value == null){
+            return '';
+        }
+        else{
+            return (new Carbon($value))->timezone('Asia/Jakarta')->toDateTimeString();
+        }
+    }
+
+    public function getTokenAttribute($value)
+    {
+        if($value == null){
+            return '';
+        }
+        else{
+            return $value;
+        }
+    }
+
+    public function getPermissionsAttribute($value)
+    {
+        if($value == null){
+            return '';
+        }
+        else{
+            return $value;
+        }
+    }
+
+}
