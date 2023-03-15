d48325bec46bf26b07dbd1be7474f8036d8fb000
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2023-03-15 15:49:55 +0700

    Update pricing.blade.php

--- resources/views/backend/pricing-plan/pricing.blade.php
+++ resources/views/backend/pricing-plan/pricing.blade.php
@@ -5,34 +5,46 @@
         <h4 class="fw-bolder mt-3">PRICING PLAN</h4>
         <h2 class="fw-bold">JOIN NOW</h2>
         <div class="col-md-4">
-            <div class="card shadow" style="border: none">
+            <div class="card shadow mt-2" style="border: none">
                 <div class="card-body text-center">
                     <h1><i class='bx bx-bone'></i></h1>
                     <h5 class="my-4">BASIC</h5>
-                    <h2 class="fw-bold">FREE</h2>
-                    <p class="my-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod ullam deserunt, quam ipsam odio nisi quidem tempore officiis earum laboriosam perferendis officia soluta iusto? Corporis, nostrum minima. Autem, qui deleniti.</p>
+                    <h2 class="fw-bold mb-5">FREE</h2>
+                    <p class="my-2"> 10 Participants Max </p>
+                    <p class="my-2"> 10 Participants Max </p>
+                    <p class="my-2"> 10 Participants Max </p>
+                    <p class="my-2"> 10 Participants Max </p>
+                    <p class="my-2 mb-5"> 10 Participants Max </p>
                     <button class="btn btn-md rounded-pill px-5 text-light fw-bolder" style="background-color: #fe397a">Free</button>
                 </div>
             </div>
         </div>
         <div class="col-md-4">
-            <div class="card shadow" style="border: none">
+            <div class="card shadow mt-2" style="border: none">
                 <div class="card-body text-center">
                     <h1><i class='bx bx-bone'></i></h1>
                     <h5 class="my-4">BASIC</h5>
-                    <h2 class="fw-bold">FREE</h2>
-                    <p class="my-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod ullam deserunt, quam ipsam odio nisi quidem tempore officiis earum laboriosam perferendis officia soluta iusto? Corporis, nostrum minima. Autem, qui deleniti.</p>
+                    <h2 class="fw-bold mb-5">FREE</h2>
+                    <p class="my-2"> 10 Participants Max </p>
+                    <p class="my-2"> 10 Participants Max </p>
+                    <p class="my-2"> 10 Participants Max </p>
+                    <p class="my-2"> 10 Participants Max </p>
+                    <p class="my-2 mb-5"> 10 Participants Max </p>
                     <button class="btn btn-md rounded-pill px-5 text-light fw-bolder" style="background-color: #10bb87">Purchase</button>
                 </div>
             </div>
         </div>
         <div class="col-md-4">
-            <div class="card shadow" style="border: none">
+            <div class="card shadow mt-2" style="border: none">
                 <div class="card-body text-center">
                     <h1><i class='bx bx-bone'></i></h1>
                     <h5 class="my-4">BASIC</h5>
-                    <h2 class="fw-bold">FREE</h2>
-                    <p class="my-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod ullam deserunt, quam ipsam odio nisi quidem tempore officiis earum laboriosam perferendis officia soluta iusto? Corporis, nostrum minima. Autem, qui deleniti.</p>
+                    <h2 class="fw-bold mb-5">FREE</h2>
+                    <p class="my-2"> 10 Participants Max </p>
+                    <p class="my-2"> 10 Participants Max </p>
+                    <p class="my-2"> 10 Participants Max </p>
+                    <p class="my-2"> 10 Participants Max </p>
+                    <p class="my-2 mb-5"> 10 Participants Max </p>
                     <button class="btn btn-md rounded-pill px-5 text-light fw-bolder" style="background-color: #5d78ff">Purchase</button>
                 </div>
             </div>
