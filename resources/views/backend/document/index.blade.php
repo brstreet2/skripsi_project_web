e45ff032dc8ffd593bf35e2a1786eac82eac25d3
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2023-03-24 11:58:57 +0700

    Update index.blade.php

--- resources/views/backend/document/index.blade.php
+++ resources/views/backend/document/index.blade.php
@@ -9,10 +9,12 @@
         <div class="card" style="border: none; background-color: #fcfcfc; border-radius: .5rem">
             <div class="card-body">
                 <a href="{{ route('document.create') }}">
-                    <button class="btn btn-primary fw-bolder mb-4 px-4 rounded-pill" style="background-color: #444EFF">Add New</button>
+                    <button class="btn btn-primary fw-bolder mb-4 px-4 rounded-pill" style="background-color: #444EFF">Add
+                        New</button>
                 </a>
                 <a href="#">
-                    <button class="btn btn-danger fw-bolder mb-4 px-4 rounded-pill" style="background-color:#E80015" id="deleteBulk">Delete
+                    <button class="btn btn-danger fw-bolder mb-4 px-4 rounded-pill" style="background-color:#E80015"
+                        id="deleteBulk">Delete
                         Bulk</button>
                 </a>
                 <table id="documentTable" class="table table-borderless text-center" style="width: 100%">
@@ -59,11 +61,25 @@
                         selectRow: true
                     }
                 }],
-                columns: [
-                  {data: 'id', name: 'id', visible: true},
-                  {data: 'name', name: 'name'},
-                  { data: 'description', name: 'description'},
-                  { data: 'action', name: 'action', orderable: false, searchable: false}
+                columns: [{
+                        data: 'id',
+                        name: 'id',
+                        visible: true
+                    },
+                    {
+                        data: 'name',
+                        name: 'name'
+                    },
+                    {
+                        data: 'description',
+                        name: 'description'
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: false
+                    }
                 ],
                 select: {
                     style: 'multi'
@@ -137,66 +153,65 @@
             });
 
             $(document).on('click', '#deleteButton', function() {
-              var thisData = $('#deleteButton');
-              Swal.fire({
-                  title: 'Remove ' + thisData.data('name') + '?',
-                  text: 'This data will be permanently removed from our system.',
-                  icon: 'warning',
-                  showCancelButton: true,
-                  confirmButtonColor: '#3085d6',
-                  cancelButtonColor: '#d33',
-                  confirmButtonText: 'Yes',
-                  width: '28em',
-                  customClass: {
-                      confirmButton: 'px-5 btn btn-sm',
-                      cancelButton: 'px-5 btn btn-sm'
-                  }
-              }).then((result) => {
-                  if(result.isConfirmed) {
-                      var url = "{{ route('document.destroy', ":id") }}";
-                      url = url.replace(':id', thisData.data('id'));
-                      $.ajax({
-                          headers: {
-                              'X-CSRF-TOKEN': $('meta[name="csrf-token"]')
-                                  .attr('content')
-                          },
-                          method: 'DELETE',
-                          url: url,
-                          success: function(data) {
-                              table.ajax.reload();
-                              Swal.fire({
-                                  position: 'center',
-                                  icon: 'success',
-                                  title: 'Data successfully deleted!',
-                                  showConfirmButton: false,
-                                  timer: 15004,
-                                  width: '28em',
-                              })
-                          },
-                          error: function(data) {
-                              console.log("Error Status: ", data.status);
-                              Swal.fire({
-                                  icon: 'error',
-                                  title: 'Oops...',
-                                  text: 'There was an error!',
-                                  footer: '<a>Try again later ...</a>',
-                                  width: '28em'
-                              })
-                          }
-                      });
-                  } else {
-                      Swal.fire({
-                          position: 'center',
-                          icon: 'error',
-                          title: 'Cancelled',
-                          showConfirmButton: false,
-                          timer: 1500,
-                          width: '28em',
-                      })
-                  }
-              });
+                var thisData = $('#deleteButton');
+                Swal.fire({
+                    title: 'Remove ' + thisData.data('name') + '?',
+                    text: 'This data will be permanently removed from our system.',
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes',
+                    width: '28em',
+                    customClass: {
+                        confirmButton: 'px-5 btn btn-sm',
+                        cancelButton: 'px-5 btn btn-sm'
+                    }
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        var url = "{{ route('document.destroy', ':id') }}";
+                        url = url.replace(':id', thisData.data('id'));
+                        $.ajax({
+                            headers: {
+                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]')
+                                    .attr('content')
+                            },
+                            method: 'DELETE',
+                            url: url,
+                            success: function(data) {
+                                table.ajax.reload();
+                                Swal.fire({
+                                    position: 'center',
+                                    icon: 'success',
+                                    title: 'Data successfully deleted!',
+                                    showConfirmButton: false,
+                                    timer: 15004,
+                                    width: '28em',
+                                })
+                            },
+                            error: function(data) {
+                                console.log("Error Status: ", data.status);
+                                Swal.fire({
+                                    icon: 'error',
+                                    title: 'Oops...',
+                                    text: 'There was an error!',
+                                    footer: '<a>Try again later ...</a>',
+                                    width: '28em'
+                                })
+                            }
+                        });
+                    } else {
+                        Swal.fire({
+                            position: 'center',
+                            icon: 'error',
+                            title: 'Cancelled',
+                            showConfirmButton: false,
+                            timer: 1500,
+                            width: '28em',
+                        })
+                    }
+                });
             });
         });
-
     </script>
 @endpush
