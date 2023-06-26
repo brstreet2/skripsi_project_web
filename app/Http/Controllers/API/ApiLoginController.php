72ab8b8bad1f18f4357dba90063fc1c7d57b59b2
Azka Secio <83747032+brstreet2@users.noreply.github.com>
2023-06-26 10:04:04 +0700

    Login API

---
+++ app/Http/Controllers/API/ApiLoginController.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Mail\activationEmail;
+use App\Models\Auth\User;
+use Cartalyst\Sentinel\Laravel\Facades\Activation;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Mail;
+use Psr\Http\Message\ServerRequestInterface;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Passport\Exceptions\OAuthServerException;
+
+class ApiLoginController extends Controller
+{
+    public function login(ServerRequestInterface $request)
+    {
+        $error         = true;
+        $expires_in    = 0;
+        $investorAccount = 0;
+        $status        = 403;
+        $message       = "";
+        $issuedToken   = "";
+        $data          = "";
+
+        try {
+            $validator = Validator::make($request->getParsedBody(), [
+                'username'       => 'required|email',
+                'password'       => 'required',
+                'client_id'      => 'required|numeric',
+                'client_secret'  => 'required',
+                'grant_type'     => 'required',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'error'     => true,
+                    'message'   => 'Invalid input',
+                    'data'      => '',
+                    'status'    => 403
+                ], 403);
+            }
+
+            if ($request->getParsedBody()['grant_type'] != 'password') {
+                return response()->json([
+                    'error'     => true,
+                    'message'   => 'Invalid input',
+                    'data'      => '',
+                    'status'    => 403
+                ], 403);
+            }
+
+            $username  = $request->getParsedBody()['username'];
+            $client_id = $request->getParsedBody()['client_id'];
+
+            if (isset($request->getParsedBody()['firebase_device_token']) == false) {
+                $firebase_device_token = '';
+            } else {
+                $firebase_device_token = $request->getParsedBody()['firebase_device_token'];
+            }
+
+            //get user
+            $user = User::where('email', $username)->firstOrFail();
+
+            //check role
+            if ($user->user_role) {
+                $activation = Activation::where('user_id', $user->id)->first();
+
+                if ($user->user_role->role->slug == 'investor') {
+                    $investorAccount = 1;
+                }
+
+                if ($investorAccount == 0) {
+                    $message = 'username/password is invalid';
+                } else {
+                    if (!$activation) {
+                        $message = 'Your account is not active.';
+                    } else {
+                        if ($activation->completed == 0) {
+                            Mail::to($user->email)->send(new activationEmail($user, $activation->code));
+                            $message = 'Your account is not active. Check your email for activation.';
+                        } else {
+                            //issuetoken
+                            $tokenResponse = parent::issueToken($request);
+
+                            //convert response to json string
+                            $content = $tokenResponse->content();
+
+                            //convert json to array
+                            $data = json_decode($content, true);
+
+                            if ($user->email != 'investor@admin.com') {
+                                //get previous user token
+                                $userTokens = $user->tokens;
+
+                                //revoke previous token
+                                if ($client_id == "2") {  //for android device
+                                    foreach ($userTokens as $key => $token) {
+                                        //revoke token except the first one
+                                        if ($token->client_id == "2" && $key !== 0) {
+                                            $token->revoke();
+                                        }
+                                    }
+                                }
+                            }
+
+                            $user->last_login            = date('Y-m-d H:i:s');
+                            $user->firebase_device_token = $firebase_device_token;
+                            $user->updated_by            = $user->name;
+                            $user->save();
+
+                            //set data for response success
+                            $error       = false;
+                            $message     = 'success';
+                            $status      = 200;
+                            $issuedToken = $data['access_token'];
+                            $expires_in  = $data['expires_in'];
+                            $data        = [
+                                'id'    => $user->id,
+                                'email' => $user->email,
+                                'name'  => $user->name,
+                                'phone' => $user->phone,
+                                'saldo' => $user->saldo
+                            ];
+                        }
+                    }
+                }
+            } else {
+                $message = 'username/password is invalid';
+            }
+
+            return response()->json([
+                'error'        => $error,
+                'message'      => $message,
+                'data'         => $data,
+                'status'       => $status,
+                'token'        => $issuedToken,
+                'expires_in'   => $expires_in,
+            ], $status);
+        } catch (ModelNotFoundException $e) { // email notfound
+            //return error message
+            $message = "username/password is invalid";
+
+            return Response::json([
+                "error"      => $error,
+                "message"    => $message,
+                "data"       => $data,
+                "status"     => $status,
+                "token"      => $issuedToken,
+                "expires_in" => $expires_in,
+            ], $status);
+        } catch (OAuthServerException $e) { //password not correct..token not granted
+            //return error message
+            $message = $e->getMessage();
+
+            return Response::json([
+                "error"      => $error,
+                "message"    => $message,
+                "data"       => $data,
+                "status"     => $status,
+                "token"      => $issuedToken,
+                "expires_in" => $expires_in,
+            ], $status);
+        } catch (Exception $e) {
+            //return error message
+            if ($e->getCode() == 10) {
+                $message = "username/password is invalid";
+            } else {
+                $message = $e->getMessage();
+            }
+
+            return Response::json([
+                "error"      => $error,
+                "message"    => $message,
+                "data"       => $data,
+                "status"     => $status,
+                "token"      => $issuedToken,
+                "expires_in" => $expires_in,
+            ], $status);
+        }
+    }
+}
