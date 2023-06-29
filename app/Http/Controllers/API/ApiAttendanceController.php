245622e8d89ade10a1b087366f3aa9a108e22c28
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2023-06-29 21:42:46 +0700

    Update ApiAttendanceController.php

--- app/Http/Controllers/API/ApiAttendanceController.php
+++ app/Http/Controllers/API/ApiAttendanceController.php
@@ -78,30 +78,38 @@ class ApiAttendanceController extends Controller
 
                 $user       = User::find($userId);
 
-                $validator  = Validator::make($request->all(), [
-                    'date'      => 'required',
-                ]);
+                // $validator  = Validator::make($request->all(), [
+                //     'date'      => 'required',
+                // ]);
 
-                if ($validator->fails()) {
-                    return response()->json([
-                        'error'     => true,
-                        'message'   => 'Invalid Input',
-                        'data'      => '',
-                        'status'    => 403
-                    ], 403);
-                }
+                // if ($validator->fails()) {
+                //     return response()->json([
+                //         'error'     => true,
+                //         'message'   => 'Invalid Input',
+                //         'data'      => '',
+                //         'status'    => 403
+                //     ], 403);
+                // }
 
-                $employee_attendance_id     = '';
+                $employee_attendance_id     = $request->attendance_id;
                 $employee_attendance_date   = $request->date;
                 $employee_clock_in          = $request->clock_in;
                 $employee_clock_out         = $request->clock_out;
 
-                $employeeAttendanceDb               = EmployeeAttendance::where('employee_id', $user->id)->where('period', $request->period)->first();
+                $employeeAttendanceDb               = EmployeeAttendance::where('employee_id', $user->id)->where('period', $request->date)->first();
 
                 if ($employeeAttendanceDb) {
-                    $employeeAttendanceDetailDb             = EmployeeAttendanceDetail::where('attendance_id', $employeeAttendanceDb->id);
+                    $employeeAttendanceDetailDb             = EmployeeAttendanceDetail::where('attendance_id', $employeeAttendanceDb->id)->first();
                     $employeeAttendanceDetailDb->clock_out  = $employee_clock_out;
                     $employeeAttendanceDetailDb->save();
+
+                    DB::commit();
+                    return response()->json([
+                        'error'     => false,
+                        'message'   => 'Attendance Recorded.',
+                        'data'      => $employeeAttendanceDetailDb,
+                        'status'    => 201
+                    ], 201);
                 } else {
                     $employeeAttendanceDb               = new EmployeeAttendance();
                     $employeeAttendanceDb->employee_id  = $user->id;
@@ -119,15 +127,15 @@ class ApiAttendanceController extends Controller
                     $employeeAttendanceDetailDb->status_string  = 'Waiting for Approval';
                     $employeeAttendanceDetailDb->created_at     = Carbon::now()->format('Y-m-d H:i:s');
                     $employeeAttendanceDetailDb->save();
-                }
 
-                DB::commit();
-                return response()->json([
-                    'error'     => false,
-                    'message'   => 'Attendance Recorded.',
-                    'data'      => [$employeeAttendanceDb],
-                    'status'    => 200
-                ], 200);
+                    DB::commit();
+                    return response()->json([
+                        'error'     => false,
+                        'message'   => 'Attendance Recorded.',
+                        'data'      => $employeeAttendanceDetailDb,
+                        'status'    => 201
+                    ], 201);
+                }
             }
         } catch (\Exception $e) {
             DB::rollback();
@@ -263,7 +271,17 @@ class ApiAttendanceController extends Controller
                     return response()->json([
                         'error'   => false,
                         'message' => 'OK',
-                        'data'    => $attendanceDetailDb,
+                        'data'    => [
+                            'id'                => $attendanceDetailDb->id,
+                            'attendance_id'     => $attendanceDetailDb->attendance_id,
+                            'date'              => $attendanceDetailDb->date,
+                            'clock_in'          => $attendanceDetailDb->clock_in,
+                            'clock_out'         => $attendanceDetailDb->clock_out,
+                            'status'            => $attendanceDetailDb->status,
+                            'status_string'     => $attendanceDetailDb->status_string,
+                            'created_at'        => $attendanceDetailDb->created_at,
+                            'updated_at'        => $attendanceDetailDb->updated_at
+                        ],
                         'status' => $status
                     ], $status);
                 } else {
