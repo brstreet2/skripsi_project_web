a570d0b2c6e9c9e3021b785519f90faab9888d07
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2023-07-01 21:06:39 +0700

    validasi

--- resources/views/backend/layout/layout.blade.php
+++ resources/views/backend/layout/layout.blade.php
@@ -42,31 +42,40 @@
             <div> <a href="#" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span
                         class="nav_logo-name">TimKerjaKu</span> </a>
                 <div class="nav_list">
-                    <a href="{{ route('dashboard.index') }}"
-                        class="nav_link {{ request()->route()->named('dashboard.*')? 'active-nav': '' }}"> <i
-                            class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Beranda</span></a>
-                    <a href="/report" class="nav_link"> <i class='bx bx-user nav_icon'></i> <span
-                            class="nav_name">Report</span> </a>
-                    <a href="{{ route('company.index') }}"
-                        class="nav_link {{ request()->route()->named('company.*')? 'active-nav': '' }}"> <i
-                            class='bx bx-buildings nav-icon'></i> <span class="nav_name">Profil Bisnis</span> </a>
-                    <a href="{{ route('document.index') }}"
-                        class="nav_link {{ request()->route()->named('document.*')? 'active-nav': '' }}"> <i
-                            class='bx bx-file nav_icon'></i> <span class="nav_name">Dokumen</span> </a>
-                    <a href="{{ route('employee.index') }}"
-                        class="nav_link {{ request()->route()->named('employee.*')? 'active-nav': '' }}"> <i
-                            class='bx bx-user nav_icon'></i> <span class="nav_name">Karyawan</span> </a>
-                    <a href="{{ route('payroll.index') }}"
-                        class="nav_link {{ request()->route()->named('payroll.*')? 'active-nav': '' }}">
-                        <i class='bx bx-bookmark nav_icon'></i> <span class="nav_name">Slip Gaji</span> </a>
-                    <a href="{{ route('attendance.index') }}"
-                        class="nav_link {{ request()->route()->named('attendance.*')? 'active-nav': '' }}"> <i
-                            class='bx bx-folder nav_icon'></i> <span class="nav_name">Kehadiran</span> </a>
-                    <a href="/announcement" class="nav_link"> <i class='bx bx-bar-chart-alt-2 nav_icon'></i> <span
-                            class="nav_name">Pengumuman</span> </a>
-                    <a href="{{ route('pricing.index') }}"
-                        class="nav_link {{ request()->route()->named('pricing.*')? 'active-nav': '' }}"> <i
-                            class='bx bx-dollar-circle nav_icon'></i> <span class="nav_name">Upgrade</span> </a>
+                    @if (Sentinel::getUser()->company != null)
+                        <a href="{{ route('dashboard.index') }}"
+                            class="nav_link {{ request()->route()->named('dashboard.*')? 'active-nav': '' }}"> <i
+                                class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Beranda</span></a>
+                        <a href="/report" class="nav_link"> <i class='bx bx-user nav_icon'></i> <span
+                                class="nav_name">Report</span> </a>
+                        <a href="{{ route('company.index') }}"
+                            class="nav_link {{ request()->route()->named('company.*')? 'active-nav': '' }}"> <i
+                                class='bx bx-buildings nav-icon'></i> <span class="nav_name">Profil Bisnis</span> </a>
+                        <a href="{{ route('document.index') }}"
+                            class="nav_link {{ request()->route()->named('document.*')? 'active-nav': '' }}"> <i
+                                class='bx bx-file nav_icon'></i> <span class="nav_name">Dokumen</span> </a>
+                        <a href="{{ route('employee.index') }}"
+                            class="nav_link {{ request()->route()->named('employee.*')? 'active-nav': '' }}"> <i
+                                class='bx bx-user nav_icon'></i> <span class="nav_name">Karyawan</span> </a>
+                        <a href="{{ route('payroll.index') }}"
+                            class="nav_link {{ request()->route()->named('payroll.*')? 'active-nav': '' }}">
+                            <i class='bx bx-bookmark nav_icon'></i> <span class="nav_name">Slip Gaji</span> </a>
+                        <a href="{{ route('attendance.index') }}"
+                            class="nav_link {{ request()->route()->named('attendance.*')? 'active-nav': '' }}"> <i
+                                class='bx bx-folder nav_icon'></i> <span class="nav_name">Kehadiran</span> </a>
+                        <a href="/announcement" class="nav_link"> <i class='bx bx-bar-chart-alt-2 nav_icon'></i> <span
+                                class="nav_name">Pengumuman</span> </a>
+                        <a href="{{ route('pricing.index') }}"
+                            class="nav_link {{ request()->route()->named('pricing.*')? 'active-nav': '' }}"> <i
+                                class='bx bx-dollar-circle nav_icon'></i> <span class="nav_name">Upgrade</span> </a>
+                    @else
+                        <a href="{{ route('dashboard.index') }}"
+                            class="nav_link {{ request()->route()->named('dashboard.*')? 'active-nav': '' }}"> <i
+                                class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Beranda</span></a>
+                        <a href="{{ route('company.index') }}"
+                            class="nav_link {{ request()->route()->named('company.*')? 'active-nav': '' }}"> <i
+                                class='bx bx-buildings nav-icon'></i> <span class="nav_name">Profil Bisnis</span> </a>
+                    @endif
                 </div>
             </div>
             <a href="{{ route('auth.logout') }}" class="nav_link"> <i class='bx bx-log-out nav_icon'></i> <span
